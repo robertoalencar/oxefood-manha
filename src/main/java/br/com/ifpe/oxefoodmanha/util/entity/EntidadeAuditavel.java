fdb3863bba99e61e4ee302cfb9c6a6f3c62c8b40
Roberto Alencar <rroberto.alencar@gmail.com>
2022-10-06 14:06:26 -0300

    enviando arquivos criados na aula 03

--- src/main/java/br/com/ifpe/oxefoodmanha/util/entity/EntidadeAuditavel.java
+++ src/main/java/br/com/ifpe/oxefoodmanha/util/entity/EntidadeAuditavel.java
@@ -2,14 +2,11 @@ package br.com.ifpe.oxefoodmanha.util.entity;
 
 import java.time.LocalDate;
 
-import javax.persistence.MappedSuperclass;
-
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
-@MappedSuperclass
 public abstract class EntidadeAuditavel extends EntidadeNegocio {
 
     private static final long serialVersionUID = -4937477043538727251L;
--- src/main/java/br/com/ifpe/oxefoodmanha/util/entity/EntidadeNegocio.java
+++ src/main/java/br/com/ifpe/oxefoodmanha/util/entity/EntidadeNegocio.java
@@ -2,15 +2,12 @@ package br.com.ifpe.oxefoodmanha.util.entity;
 
 import java.io.Serializable;
 
-import javax.persistence.MappedSuperclass;
-
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
-@MappedSuperclass
 @EqualsAndHashCode(of = { "id" })
 public class EntidadeNegocio implements Serializable {
 
