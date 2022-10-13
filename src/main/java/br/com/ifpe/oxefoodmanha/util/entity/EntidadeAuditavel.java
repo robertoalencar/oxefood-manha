be21d168c2fa21f7bbf93467e728b161ada0ac23
Roberto Alencar <rroberto.alencar@gmail.com>
2022-10-13 16:08:35 -0300

    implementando a funcionalidade de categoria de produto

--- src/main/java/br/com/ifpe/oxefoodmanha/util/entity/EntidadeAuditavel.java
+++ src/main/java/br/com/ifpe/oxefoodmanha/util/entity/EntidadeAuditavel.java
@@ -2,23 +2,43 @@ package br.com.ifpe.oxefoodmanha.util.entity;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Version;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@MappedSuperclass
 public abstract class EntidadeAuditavel extends EntidadeNegocio {
 
-    private static final long serialVersionUID = -4937477043538727251L;
+    private static final long serialVersionUID = -501412849720642170L;
 
+    @JsonIgnore
+    @Version
     private Long versao;
 
+    @JsonIgnore
+    @CreatedDate
     private LocalDate dataCriacao;
 
+    @JsonIgnore
+    @LastModifiedDate
     private LocalDate dataUltimaModificacao;
 
+    @JsonIgnore
+    @Column
     private Long criadoPor; // Id do usuário que o criou
 
-    private Long ultimaModificacaoPor;
+    @JsonIgnore
+    @Column
+    private Long ultimaModificacaoPor; // Id do usuário que fez a última alteração
     
 }
