dcff674dcaa54e0936dd4f4c53413c47c754a57c
Roberto Alencar <rroberto.alencar@gmail.com>
2022-10-13 20:26:25 -0300

    implementando projeto

--- src/main/java/br/com/ifpe/oxefoodmanha/OxefoodManhaApplication.java
+++ src/main/java/br/com/ifpe/oxefoodmanha/OxefoodManhaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OxefoodManhaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OxefoodManhaApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(OxefoodManhaApplication.class, args);
+    }
 
 }
